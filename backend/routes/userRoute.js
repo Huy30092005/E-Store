40ddd96438a0c58541f412b65259a0e7b4ada2f4
Trần Thị Hoài <tranhoai194205@gmail.com>
2026-05-05 17:35:47 +0700

    add google auth route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,6 +3,7 @@ import {
   loginUser,
   registerUser,
   adminLogin,
+  googleAuth,
   getCurrentUser,
   listUsers,
 } from "../controllers/userController.js";
@@ -14,6 +15,7 @@ const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.post("/admin", adminLogin);
+userRouter.post("/google-auth", googleAuth);
 userRouter.get("/me", authUser, getCurrentUser);
 userRouter.get("/list", adminAuth, listUsers);
 
